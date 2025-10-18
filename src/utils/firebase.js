cff38309d72eca1e7b7a912b1a5246da6a61dcfe
Ritesh <riteshjha24060@gmail.com>
2025-10-18 12:36:33 +0530

    Update firebase SDK Code

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmZm0iaVNXG6L7qhUJ-JbiAVz3yqDAI_8",
-  authDomain: "cineo-d7155.firebaseapp.com",
-  projectId: "cineo-d7155",
-  storageBucket: "cineo-d7155.firebasestorage.app",
-  messagingSenderId: "52272249824",
-  appId: "1:52272249824:web:c04abddf1254d195c1c985",
+  apiKey: "AIzaSyCKY20cmuTYX_-JCOUQwL6VIMOykFMY5sU",
+  authDomain: "cineverse-3fa33.firebaseapp.com",
+  projectId: "cineverse-3fa33",
+  storageBucket: "cineverse-3fa33.firebasestorage.app",
+  messagingSenderId: "969956156005",
+  appId: "1:969956156005:web:ecff4bf5b57cc615b59c8c",
+  measurementId: "G-5FFRKBXGXR"
 };
 
 // Initialize Firebase
